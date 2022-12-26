0b301e6ac321109cd4e03e4ccad8d57d28b0ad53
Divyanshu Mahajan <divyanshumahajan98@gmail.com>
2022-12-27 01:52:24 +0530

    Configured react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import "./App.css";
-import Home from "./Components/Home";
-import NavBar from "./Components/NavBar";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './Components/Home';
+import Checkout from './Components/Checkout';
+import Layout from './Components/Layout';
+
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/checkout',
+        element: <Checkout />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,52 +1,53 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function () {
+export default function NavBar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary py-3 shadow-sm">
-        <div class="container">
-          <a class="navbar-brand fw-bold fs-4" href="#">
+      <nav class='navbar navbar-expand-lg bg-body-tertiary py-3 shadow-sm'>
+        <div class='container'>
+          <a class='navbar-brand fw-bold fs-4' href='#'>
             Bakery Home
           </a>
           <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            class='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span class="navbar-toggler-icon"></span>
+            <span class='navbar-toggler-icon'></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
+          <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul class='navbar-nav mx-auto mb-2 mb-lg-0'>
+              <li class='nav-item'>
+                <Link class='nav-link active' aria-current='page' to='/'>
                   Home
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
                   Products
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
                   About
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li class='nav-item'>
+                <a class='nav-link' href='#'>
                   Contact Us
                 </a>
               </li>
             </ul>
-            <div className="buttons">
-              <a href="" className="btn btn-outline-dark">
-                <i className="fa fa-shopping-cart me-1 ms-2"></i>
+            <div className='buttons'>
+              <Link to='/checkout' className='btn btn-outline-dark'>
+                <i className='fa fa-shopping-cart me-1 ms-2'></i>
                 Cart(0)
-              </a>
+              </Link>
             </div>
           </div>
         </div>
